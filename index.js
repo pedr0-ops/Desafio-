f4554795c439c1f3f640fcf8ebdbffe1869bdbcb
pedr0-ops <pedro402018@outlook.com>
2022-06-08 11:38:03 -0300

    refactoring code

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
-const fetch = require("node-fetch");
-const CEPS = [59920000, 63430000, 63460000, 59930000, 63475000];
-const options = {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'default',
-}
+import fetch from "node-fetch";
+const CEPS = ['62980000', '63960000', '62900000', '63800000',
+    '60025110', '62014102', '76870631', '53416420',
+    '59084145', '60175551', '69104368', '51170901',
+    '73005522', '76876702', '11702160'];
 
 async function getInformationCEP(CEP) {
     try {
-        let response = await fetch(`https://viacep.com.br/ws/${CEP}/json/`, options);
-        let CEPData = await response.json();
-        return CEPData;
-    } catch (error) {
-        console.log("Erro" + error)
-    }
-
+        let response = await fetch(`https://viacep.com.br/ws/${CEP}/json/`);
+        if (!response.ok) { throw new Error(response.status); }
+        let CEPsData = await response.json();
+        return CEPsData;
+    } catch (error) { console.error(error); }
 }
 
-async function writeInformationCEPs(listCEPs) {
-    for (var i = 0; i < listCEPs.length; i++) {
-        await delay(3);
-        console.log(await getInformationCEP(listCEPs[i]));
-    }
+function showInformationCEPs(CEPs) {
+    console.time("time");
+    CEPs.map(async cep => { console.log(await getInformationCEP(cep)); })
+    console.timeEnd("time");
 }
 
-function delay(seconds) {
-    return new Promise((resolve => {
-        setTimeout(resolve, seconds * 1000)
-    }))
-}
-
-writeInformationCEPs(CEPS);
+showInformationCEPs(CEPS);
 
 
 
